30b4722e8183b3571cc8931ad998d4b50eda65eb
tatianibassani <tatianibassani@gmail.com>
2022-10-26 23:34:53 +0000

    implementing Stdin

--- play.js
+++ play.js
@@ -22,3 +22,23 @@ const connect = function () {
 
 console.log("Connecting ...");
 connect;
+
+// setup interface to handle user input from stdin
+
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.on("data", handleUserInput);
+  stdin.resume();
+  return stdin;
+};
+
+const handleUserInput = function (key) {
+  if (key === '\u0003') {
+    process.exit();
+  }
+
+};
+
+setupInput();
