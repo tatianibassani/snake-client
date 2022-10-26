4bbf64fef7b0dab92bf6c83b194ffeb8c8e36200
tatianibassani <tatianibassani@gmail.com>
2022-10-26 19:39:02 +0000

    creating a play

---
+++ client.js
@@ -0,0 +1,8 @@
+const connect = function () {
+  const conn = net.createConnection({
+    host: '10.0.2.15',
+    port: 50541
+  
+  });
+}
+module.exports = connect;
---
+++ play.js
@@ -0,0 +1,22 @@
+const net = require("net");
+
+// establishes a connection with the game server
+const connect = function () {
+  const conn = net.createConnection({
+    host: '10.0.2.15',
+    port: 50541
+  });
+
+  // interpret incoming data as text
+  conn.setEncoding("utf8");
+
+  conn.on("connect", () => {
+    console.log("you ded cuz you idled");
+
+  });
+
+  return conn;
+};
+
+console.log("Connecting ...");
+connect();
