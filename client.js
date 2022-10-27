70a1a60e6149ccad273761d86f684b8414ac0c05
tatianibassani <tatianibassani@gmail.com>
2022-10-27 03:30:54 +0000

    implementing Constants Module

--- client.js
+++ client.js
@@ -1,12 +1,13 @@
 const net = require("net");
+const { IP, PORT } = require("./constants");
 //const readLine = require("readLine");
 
 // establishes a connection with the game server
 const connect = function() {
 
   const conn = net.createConnection({
-    host: '10.0.2.15',
-    port: 50541
+    host: IP,
+    port: PORT
   });
 
   // interpret incoming data as text
