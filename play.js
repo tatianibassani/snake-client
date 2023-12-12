985ba70cee72cf46d4d921ea725574897a4fc0f4
Tatiana <tatianibassani@gmail.com>
2023-12-12 20:53:32 +0000

    creating client file and exporting from play

--- play.js
+++ play.js
@@ -1,21 +1,4 @@
-const net = require("net");
-
-// establishes a connection with the game server
-const connect = function () {
-  const conn = net.createConnection({
-    host: "localhost",
-    port: "50541",
-  });
-
-  // interpret incoming data as text
-  conn.setEncoding("utf8");
-    conn.on("connect", () => {
-    console.log("you ded cuz you idled");
-
-  });
-  return conn;
-};
-
-console.log("Connecting ...");
-connect();
+const { connect } = require('./client')
+const { connect } = require('./input')
+// console.log("Connecting ...");
 
