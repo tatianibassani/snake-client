44fc3027c2f71a00bb723875976622da376682bb
Tatiana <tatianibassani@gmail.com>
2023-12-18 03:36:34 +0000

    improvments

--- input.js
+++ input.js
@@ -12,34 +12,43 @@ const setupInput = function (conn) {
   return stdin;
 };
 
+const MOVES = {
+  w: "Move: up",
+  a: "Move: left",
+  s: "Move: down",
+  d: "Move: right",
+  q: "Say: go long",
+  e: "Say: snake bite",
+};
+
 const handleUserInput = function (key) {
   if (key === '\u0003') {
     process.exit();
   }
 
   if (key === 'w') {
-    connection.write('Move: up');
-  }
+  connection.write(MOVES.w);
+}
 
-  if (key === 's') {
-    connection.write('Move: down');
-  }
+if (key === "a") {
+  connection.write(MOVES.a);
+}
 
-  if (key === 'a') {
-    connection.write('Move: left');
-  }
+if (key === "s") {
+  connection.write(MOVES.s);
+}
 
-  if (key === 'd') {
-    connection.write('Move: right');
-  }
+if (key === "d") {
+  connection.write(MOVES.d);
+}
 
-  if (key === 'r') {
-    connection.write('Say: hello');
-  }
+if (key === "q") {
+  connection.write(MOVES.q);
+}
 
-  if (key === 'p') {
-    connection.write('Say: Howdy!');
-  }
+if (key === "e") {
+  connection.write(MOVES.e);
+}
 };
 
 //setupInput();
